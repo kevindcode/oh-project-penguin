7d3b1309b32606eeed5941e018e25b00bcf8e649
Andrea Fiorucci <hgames.andy@gmail.com>
2019-06-07 15:07:45 +0100

    Added pulsing behaviour to the magnet object. Added rotating particle around the magnet object

--- src/index.js
+++ src/index.js
@@ -8,16 +8,18 @@ const BACKGROUND_COLOR = '#2E4052';
 let blackhole;
 let testBall;
 
-window.setup = function() {
+window.setup = function () {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  blackhole = new BlackHole(120);
+  blackhole = new BlackHole(200, windowHeight / 2, 120, 5);
   testBall = new SpaceObject(20);
 };
 
-window.draw = function() {
+window.draw = function () {
   background(BACKGROUND_COLOR);
+
   blackhole.display();
+  blackhole.update();
 
   testBall.display();
   testBall.moveToTarget(blackhole);
