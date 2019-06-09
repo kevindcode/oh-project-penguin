c82bf679b77cafb2b9ba82db7c46d9b5675d0e5f
andyhgames <41429655+andyhgames@users.noreply.github.com>
2019-06-09 11:22:03 +0100

    Magnet Object Update (#8)
    * Added pulsing behaviour to the magnet object. Added rotating particle around the magnet object
    
    * updated to use desctructoring

--- src/BlackHole.js
+++ src/BlackHole.js
@@ -1,14 +1,51 @@
 class BlackHole {
-  constructor(radius) {
+  constructor(posX, posY, radius, radiusOffset) {
     this.radius = radius;
-    this.vector = createVector(300, window.innerHeight / 2);
+    this.radiusOffset = radiusOffset;
+    this.radiusMaxRange = this.radius + this.radiusOffset;
+    this.radiusMinRange = this.radius - this.radiusOffset;
+    this.flipPulse = true;
+    this.vector = createVector(posX, posY);
+    this.orbit = {
+      posX: this.radius + this.radiusOffset,
+      posY: this.radius + this.radiusOffset,
+      angle: 0,
+      radiusFromBlackHole: 120,
+      radius: 30
+    };
   }
 
   display() {
-    const { x, y } = this.vector;
+    const {x, y} = this.vector;
     fill('#BDD9BF');
     ellipse(x, y, this.radius);
   }
+
+  update() {
+    this.pulseAnimation();
+    this.orbitAnimation();
+  }
+
+  pulseAnimation() {
+    if (this.radius > this.radiusMaxRange) {
+      this.flipPulse = !this.flipPulse;
+    } else if (this.radius < this.radiusMinRange) {
+      this.flipPulse = !this.flipPulse;
+    }
+    let increment = this.flipPulse ? 0.2 : -0.2;
+    this.radius += increment;
+  }
+
+  orbitAnimation() {
+    translate(this.vector.x, this.vector.y);
+    this.orbit.posX = this.orbit.radiusFromBlackHole * Math.cos(this.orbit.angle);
+    this.orbit.posY = this.orbit.radiusFromBlackHole * Math.sin(this.orbit.angle);
+    fill(76, 219, 200, 100);
+    ellipse(this.orbit.posX, this.orbit.posY, this.orbit.radius);
+    this.orbit.angle += 0.01;
+    resetMatrix();
+  }
+
 }
 
 export default BlackHole;
